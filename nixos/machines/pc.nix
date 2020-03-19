fdf58ff4aebfc1db062663b0f998b3fc16bd888b
sheepfleece <rowanmorrison@protonmail.ch>
2020-03-19 05:06:41 +0300

    add pc

---
+++ nixos/machines/pc.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }:
+
+{ 
+  # Use the GRUB 2 boot loader
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/sda";
+
+  services.openssh = {
+    enable = true;
+    listenAddresses =
+      [ { addr = "192.168.0.100"; port = 22; } ];
+    authorizedKeysFiles = [ "/root/.ssh/authorized_keys" ];
+  };
+  virtualisation.docker = {
+    enable = true;
+  };
+
+}
